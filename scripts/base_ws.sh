0384a2dc4b3d0976df4e7d2ba4753ae1aa6aed73
amiz <grofitrnd@gmail.com>
2025-03-06 01:21:06 +0200

    adding_scripts

--- scripts/base_ws.sh
+++ scripts/base_ws.sh
@@ -3,15 +3,15 @@
 # get the current directory
 CURRENT_DIR=$(pwd)
 # check if the last directory is "mini_agro"
-if [[ "${CURRENT_DIR##*/}" != "mini_agro" ]]; then
-  echo "Error: The current directory's last pack must be 'mini_agro'."
+if [[ "${CURRENT_DIR##*/}" != "miniagro" ]]; then
+  echo "Error: The current directory's last pack must be 'miniagro'."
   return 1
 fi
 
-echo $CURRENT_DIR/_env/mini_agro/bin/activate
+echo $CURRENT_DIR/_env/miniagro/bin/activate
 
 # activate the Python virtual environment
-source $CURRENT_DIR/_env/mini_agro/bin/activate
+source $CURRENT_DIR/_env/miniagro/bin/activate
 
 # build the Python path with the current directory and API server subdirectory
 export GROFIT_BASE_DIR=$CURRENT_DIR
